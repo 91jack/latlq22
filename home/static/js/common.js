efcdeb08d299c8ad41abb42c28c93a84f805d20c
91jack <2764132626@qq.com>
2018-08-31 18:23:27 +0800

    1
    1

--- home/static/js/common.js
+++ home/static/js/common.js
@@ -13,35 +13,3 @@
 	};
 })();
 
-
- // 顶部导航切换效果
-$('.header .nav>a').hover(function() {
-	$(this).addClass('active').siblings().removeClass('active');
-	var imgUrl = $(this).children('img').attr('src');
-	var newImgUrl = imgUrl.replace(/1/, "2");
-	$(this).children('img').attr('src', newImgUrl);
-}, function() {
-	var imgUrl = $(this).children('img').attr('src');
-	var newImgUrl = imgUrl.replace(/2/, "1");
-	$(this).children('img').attr('src', newImgUrl);
-})
-
-// 二级导航
-$('#tongji').hover(function(){
-	$('.subnav').slideDown(200);
-},function(){
-	setTimeout(function(){
-		$('.subnav').slideUp(200);
-	},3000)
-})
-
-// 顶部导航显示隐藏	
-var on = true;
-$('.top-title h1').on('click', function() {
-	if(on) {
-		$('.header').hide();
-	} else {
-		$('.header').show();
-	}
-	on = !on;
-})
